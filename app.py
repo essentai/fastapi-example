d252c19af19438fa5f7e76b6f491dff93bd4dbfd
Essentai <xbv-0728@gmail.com>
2022-09-25 13:43:09 +0600

    added new lines

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get("/")
 def read_root():
     return { "message": "Hello world!" }
 
+@app.get("/about")
+def about():
+    return { "say": "I have nothing to say" }
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished works"}
+
+@app.get("/blog/{blog_id}")
+def blog(blog_id: int):
+    return {"data": blog_id}
+
+@app.get("/blog/{blog_id}/comments")
+def comments(blog_id: int):
+ if (blog_id==1):
+     return {"comment": "no bs"}
+ else:
+     return {"0"}
