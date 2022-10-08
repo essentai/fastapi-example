81475dc3769e1fc6603847a3e2af8839b4cf4d7a
Essentai <xbv-0728@gmail.com>
2022-10-08 15:08:17 +0600

    created API for empty database

--- app.py
+++ app.py
@@ -1,64 +1,74 @@
-from typing import Union, Optional
+from typing import Union, Optional, List
 from fastapi import FastAPI, Path, Query, HTTPException, status
 from pydantic import BaseModel
-
+from database import SessionLocal
+import models
 app = FastAPI()
 
 class Item(BaseModel):
+    id: int
     name: str
+    description: str
     price: float
-    brand: Optional[str] = None
+    on_offer: bool
 
-class UpdateItem(BaseModel):
-    name: Optional[str] = None
-    price: Optional[float] = None
-    brand: Optional[str] = None
+    class Config:
+        orm_mode=True
 
-class Poki(BaseModel):
-    affiliation: str
-    orientation: str
-    number: Optional[int] = None
+db=SessionLocal()
 
-@app.get("/")
-def read_root():
-    return { "message": "Hello world!" }
+@app.get("/items",response_model=List[Item],status_code=200)
+def get_all_items():
+    items=db.query(models.Item).all()
 
-@app.get("/about")
-def about():
-    return { "say": "I have nothing to say" }
+    return items
 
-inventory = {}
-paul_sanders = {}
+@app.get("/item/{item_id}",response_model=Item,status_code=status.HTTP_200_OK)
+def get_an_item(item_id:int):
+    item=db.query(models.Item).filter(models.Item.id==item_id).first()
 
-@app.get("/get-by-name")
-def get_item(*, name: Optional[str] = None, test: int):
-    for item_id in inventory:
-        if inventory[item_id].name == name:
-            return inventory[item_id]
-    raise HTTPException(status_code=404, detail="Item ID not found.")
+    return item
 
-@app.post("/create-item/{item_id}")
-def create_item(item_id: int, item: Item):
-    if item_id in inventory:
-        raise HTTPException(status_code=400, detail="Item ID already exists.")
-    inventory[item_id]= item
-    return inventory[item_id]
+@app.post("/items",response_model=Item,status_code=status.HTTP_201_CREATED)
+def create_item(item:Item):
+    db_item=db.query(models.Item).filter(models.Item.name==item.name).first()
 
-@app.put("/update-item/{item_id}")
-def update_item(item_id: int, item: UpdateItem):
-    if item_id not in inventory:
-        raise HTTPException(status_code=404, detail="Item ID not found.")
-    if item.name != None:
-        inventory[item_id].name = item.name
-    if item.price != None:
-        inventory[item_id].price = item.price
-    if item.brand != None:
-        inventory[item_id].brand = item.brand
-    return inventory[item_id]
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="Item already exists")
 
-@app.delete("/delete-item")
-def delete_item(item_id: int = Query(..., description="The IDf the item gt = 0")):
-    if item_id not in inventory:
-        raise HTTPException(status_code=404, detail="Item ID not found.")
-    del inventory[item_id]
-    return {"Success": "Item succesfully deleted."}
+    new_item=models.Item(
+        name=item.name,
+        price=item.price,
+        description=item.description,
+        on_offer=item.on_offer
+    )
+
+   
+    db.add(new_item)
+    db.commit()
+
+    return new_item
+
+@app.put("/item/{item_id}",response_model=Item,status_code=status.HTTP_200_OK)
+def update_item(item_id:int,item:Item):
+    item_to_update=db.query(models.Item).filter(models.Item.id==item_id).first()
+    item_to_update.name=item.name
+    item_to_update.price=item.price
+    item_to_update.description=item.description
+    item_to_update.on_offer=item.on_offer
+
+    db.commit()
+
+    return item_to_update
+
+@app.delete("/item/{item_id}")
+def delete_item(item_id:int):
+    item_to_delete=db.query(models.Item).filter(models.Item.id==item_id).first()
+
+    if item_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Resource Not Found")
+    
+    db.delete(item_to_delete)
+    db.commit()
+    
+    return item_to_delete
